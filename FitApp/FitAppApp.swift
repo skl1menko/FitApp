6e32937454d64d0dde49b4bc85957dad5dff3107
skl1menko <alklimenko227@gmail.com>
2025-12-25 23:35:05 +0200

    Initial Commit

---
+++ FitApp/FitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitAppApp.swift
+//  FitApp
+//
+//  Created by Sasha Klymenko on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct FitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
